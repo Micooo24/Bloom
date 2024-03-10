3cdb8aaa24f4d3ef3f3dc39591f09d5487e5eb76
Micooo24 <mico.rabino24@gmail.com>
2024-03-10 11:03:55 +0800

    logo

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,3 +1,10 @@
+<style>
+    .adjustable-logo {
+        width: 50px;
+        height: 50px;
+    }
+</style>
+
 <nav x-data="{ open: false }" class="bg-white dark:bg-gray-800 border-b border-gray-100 dark:border-gray-700">
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -6,7 +13,7 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('home') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
+                        <img src="{{ asset('logo/bloom.png') }}" class="adjustable-logo" />
                     </a>
                 </div>
 
